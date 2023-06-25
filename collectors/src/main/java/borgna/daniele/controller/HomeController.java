0d2ff54ca70aac1e139aded7694d48becb740647
Borgna02 <danybor67@gmail.com>
2023-06-26 00:13:16 +0200

    Prima implementazione interfaccia

---
+++ collectors/src/main/java/borgna/daniele/controller/HomeController.java
@@ -0,0 +1,123 @@
+package borgna.daniele.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+
+    @FXML
+    private void operazione1(ActionEvent event) {
+        // Logica per l'operazione 1
+    }
+
+    @FXML
+    private void operazione2a(ActionEvent event) {
+        // Logica per l'operazione 2a
+    }
+
+    @FXML
+    private void operazione2b(ActionEvent event) {
+        // Logica per l'operazione 2b
+    }
+
+    @FXML
+    private void operazione2c(ActionEvent event) {
+        // Logica per l'operazione 2c
+    }
+
+    @FXML
+    private void operazione2d(ActionEvent event) {
+        // Logica per l'operazione 2d
+    }
+
+    @FXML
+    private void operazione2e(ActionEvent event) {
+        // Logica per l'operazione 2e
+    }
+
+    @FXML
+    private void operazione2f(ActionEvent event) {
+        // Logica per l'operazione 2f
+    }
+
+    @FXML
+    private void operazione2g(ActionEvent event) {
+        // Logica per l'operazione 2g
+    }
+
+    @FXML
+    private void operazione3a(ActionEvent event) {
+        // Logica per l'operazione 3a
+    }
+
+    @FXML
+    private void operazione3b(ActionEvent event) {
+        // Logica per l'operazione 3b
+    }
+
+    @FXML
+    private void operazione4(ActionEvent event) {
+        // Logica per l'operazione 4
+    }
+
+    @FXML
+    private void operazione5(ActionEvent event) {
+        // Logica per l'operazione 5
+    }
+
+    @FXML
+    private void operazione6(ActionEvent event) {
+        // Logica per l'operazione 6
+    }
+
+    @FXML
+    private void operazione7(ActionEvent event) {
+        // Logica per l'operazione 7
+    }
+
+    @FXML
+    private void operazione8(ActionEvent event) {
+        // Logica per l'operazione 8
+    }
+
+    @FXML
+    private void operazione9(ActionEvent event) {
+        // Logica per l'operazione 9
+    }
+
+    @FXML
+    private void operazione10(ActionEvent event) {
+        // Logica per l'operazione 10
+    }
+
+    @FXML
+    private void operazione11(ActionEvent event) {
+        // Logica per l'operazione 11
+    }
+
+    @FXML
+    private void operazione12a(ActionEvent event) {
+        // Logica per l'operazione 12a
+    }
+
+    @FXML
+    private void operazione12b(ActionEvent event) {
+        // Logica per l'operazione 12b
+    }
+
+    @FXML
+    private void operazione13(ActionEvent event) {
+        // Logica per l'operazione 13
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Puoi aggiungere qui la logica per gestire gli eventi dei bottoni
+    }
+}
